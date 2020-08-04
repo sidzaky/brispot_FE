692478865e7c7884db0de483c885b61732388ed0
Nicky Prabowo <nicky@warungpintar.co>
2020-08-04 09:19:28 +0700

    chore: Move change password modal to template

--- application/modules/cluster/views/cluster_v.php
+++ application/modules/cluster/views/cluster_v.php
@@ -468,44 +468,6 @@
 	</div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
-<!-- modal-->
-<div class="modal" id="modalz" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog modal-lg">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" onclick="$('#modalz').hide();" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h5 class="modal-title">Form klaster <?php echo $this->session->userdata('nama_uker') ?></h5>
-			</div>
-			<div class="modal-body">
-				<form>
-					<?php
-					$dker = '
-						<div class="form-group">
-							<label class="control-label">Kode Uker</label>
-							<div id="chker"></div>
-							<input type="number" class="form-control" id="kode_uker_c" onchange="getuker(this.value);" placeholder="required" value="" required>
-						</div>';
-					echo ($this->session->userdata('permission') > 1 ? $dker : '');
-					?>
-					<div class="form-group has-feedback">
-						<label class="control-label">Password Baru</label>
-						<input type="password" onchange="checkPassword()" class="form-control" placeholder="Password" name="password" id="password">
-						<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-					</div>
-					<div class="form-group has-feedback">
-						<label class="control-label">Ketik Ulang Password Baru</label>
-						<input type="password" onchange="checkPassword();" class="form-control" placeholder="Confirm Password" name="Cpassword" id="Cpassword">
-						<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-					</div>
-				</form>
-			</div>
-			<div class="modal-footer">
-				<button class="btn btn-primary waves-effect waves-light" onclick="$('#modalz').hide();">Batal</button>
-				<button class="btn btn-success waves-effect waves-light" disabled id="dsubmit">Kirim</button>
-			</div>
-		</div>
-	</div>
-</div>
 <?php if ($this->session->userdata('notif') == 1) { ?>
 	<div class="modal " id="modalnotif" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 		<div class="modal-dialog modal-lg">
--- application/views/template.php
+++ application/views/template.php
@@ -1,43 +1,33 @@
 <?php $this->load->view('header'); ?>
 
-<?php 
-
-	if( $navbar != null )
-	{
-		$this->load->view($navbar);
-	}
-
+<?php
+if ($navbar != null) {
+	$this->load->view($navbar);
+}
 ?>
 
-<?php 
-
-	if( $sidebar != null )
-	{
-		$this->load->view($sidebar);
-	}
-
+<?php
+if ($sidebar != null) {
+	$this->load->view($sidebar);
+}
 ?>
 
-<?php 
-
-	if( $content != null )
-	{
-		$this->load->view($content);
-	}
-	else
-	{
-		echo "No content available";
-	}
-
+<?php
+if ($content != null) {
+	$this->load->view($content);
+} else {
+	echo "No content available";
+}
 ?>
 
-<?php 
-	
-	if( $content != 'login' && $content != 'installer'  ) 
-	{
-		$this->load->view('footer');
-	}
-	
+<?php
+if ($content != 'login' && $content != 'installer') {
+	$this->load->view('footer');
+}
 ?>
+
+<?php $this->load->view('changePassword'); ?>
+
+<div id="changePasswordContainer"></div>
 
 <?php $this->load->view('scripts'); ?>
