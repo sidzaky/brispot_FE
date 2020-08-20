3edfb9c3fedb4cb28246475335f4fccf15e282ae
sidzaky <si.dzaky@gmail.com>
2020-08-20 20:00:39 +0700

    add setting module

---
+++ application/modules/setting/models/Setting_m.php
@@ -0,0 +1,21 @@
+
+
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class setting_m extends CI_Model 
+{
+
+   
+    public function __construct()
+    {
+            parent::__construct();
+            //Load Dependencies
+    }
+
+	
+}
+
+	
+	
+/* End of file user_m.php */
+/* Location: ./application/models/user_m.php */
