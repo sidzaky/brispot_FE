8386a2f1dcb57ba3a333bf71ac5e0399f44384c2
Nicky Prabowo <nicky@warungpintar.co>
2020-08-21 22:15:16 +0700

    chore: change font

--- application/views/header.php
+++ application/views/header.php
@@ -7,6 +7,8 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap.min.css" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/AdminLTE.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
