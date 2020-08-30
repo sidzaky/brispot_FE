14b0c217adabbf43647dd98747299259761c79b3
sidzaky <si.dzaky@gmail.com>
2020-08-30 18:48:33 +0700

    update setting

--- application/modules/setting/models/Setting_m.php
+++ application/modules/setting/models/Setting_m.php
@@ -14,27 +14,27 @@ class setting_m extends CI_Model
     
     public function get_jenisusaha_m(){
         $sql="select * from cluster_jenis_usaha";
-        return $this->db->query($sql)->result();
+        return $this->db->query($sql)->result_array();
     }
 
     public function get_jenisusahamap_m(){
         $sql="select * from cluster_jenis_usaha_map";
-        return $this->db->query($sql)->result();
+        return $this->db->query($sql)->result_array();
     }
 
     public function get_kebutuhansarana_m(){
         $sql="select * from cluster_kebutuhan_sarana";
-        return $this->db->query($sql)->result();
+        return $this->db->query($sql)->result_array();
     }
 
     public function get_kebutuhanpendidikan_m(){
         $sql="select * from cluster_kebutuhan_pendidikan_pelatihan";
-        return $this->db->query($sql)->result();
+        return $this->db->query($sql)->result_array();
     }
 
     public function get_kebutuhanskemakredit_m(){
         $sql="select * from cluster_kebutuhan_skema_kredit";
-        return $this->db->query($sql)->result();
+        return $this->db->query($sql)->result_array();
     }
 }	
 	
