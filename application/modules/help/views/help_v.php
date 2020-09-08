e1bc15ceb6bf215347e7c8fb47ceccaed373a490
sidzaky <si.dzaky@gmail.com>
2020-09-09 02:17:41 +0700

    help progress

--- application/modules/help/views/help_v.php
+++ application/modules/help/views/help_v.php
@@ -14,20 +14,27 @@
 						<button class="btn btn-success waves-effect waves-light btn-sm" onclick="getform()" type="button"><i class="fa fa-plus"></i> Tambah Data</button>
 					</div>
 				</div>
+				<script>
+					$(document).ready(function() {
+						$('#table-help').DataTable({
+							"scrollX": true,
+							"processing": true,
+							"serverSide": true,
+							"deferRender": true,
+							"ajax": {
+								"url": "./help/getdataquestion",
+								"type": "POST"
+							},
+						});
+					});
+				</script>
 				<div class="table-responsive">
-					<table id="table-cluster" class="table table-striped table-bordered" width="100%">
+					<table id="table-help" class="table table-striped" width="100%">
 						<thead>
 							<tr>
 								<th>No</th>
-								<th>Kantor Wilayah</th>
-								<th>Nama Kanca</th>
-								<th>Nama Uker</th>
-								<th>Nama Kelompok Usaha</th>
-								<th>Jml / Input Anggota </th>
-								<th>Jenis Usaha</th>
-								<th>Bentuk Produk/Jasa</th>
-								<th>Status Pengajuan</th>
-								<th>Action</th>
+								<th>Uker Penanya</th>
+								<th>Detil</th>
 							</tr>
 						</thead>
 					</table>
@@ -36,3 +43,67 @@
 		</div>
 	</section>
 </div>
+<!-- Modal -->
+<style>
+	.modal-body {
+		max-height: calc(100vh - 200px);
+		overflow-y: auto;
+	}
+
+	.btn-file {
+		position: relative;
+		overflow: hidden;
+	}
+
+	.btn-file input[type=file] {
+		position: absolute;
+		top: 0;
+		right: 0;
+		min-width: 100%;
+		min-height: 100%;
+		font-size: 100px;
+		text-align: right;
+		filter: alpha(opacity=0);
+		opacity: 0;
+		outline: none;
+		background: white;
+		cursor: inherit;
+		display: block;
+	}
+
+	.img-upload {
+		width: 100%;
+	}
+</style>
+
+<div class="modal " id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" onclick="$('#modal').hide();" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h5 class="modal-title">Form Pertanyaan</h5>
+			</div>
+			<div class="modal-body">
+				<div id="mod-content">
+                    <div class="col-sm-12">
+							<label for="thedata" class="col-sm-12 control-label">
+								<h3 align="center">Silahkan Ajukan pertanyaan</h3>
+							</label>
+						</div>
+					<form>
+                        <div class="form-group">
+                            <textarea class="form-control required" id="question" value=""></textarea>
+						</div>
+                    </form>
+                    <div class="modal-footer">
+                        <button class="btn btn-primary waves-effect waves-light" onclick="$('#modal').hide();">Batal</button>
+                        <button class="btn btn-success waves-effect waves-light" id="sbt" onclick="inputformhelp();">Kirim</button>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="./assets/js/send.js"></script>
+<script src="./assets/js/help.js"></script>
